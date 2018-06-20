20313f5ecb4b0bc68f5f92fa4b8e74a7216cce02
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-20 13:08:17 +0000

    automatically backed up by learn

--- db/migrate/20180620130712_create_preferences.rb
+++ db/migrate/20180620130712_create_preferences.rb
@@ -1,6 +1,10 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
+      t.boolean :song_sort_order
+      t.boolean :artist_sort_order
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
     end
   end
 end
