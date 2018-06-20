c84c7bc7eeef0000248b71714d55c40fa768535d
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-20 13:56:42 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -14,4 +14,11 @@ class Admin::PreferencesController < ApplicationController
   def create
     raise params
   end
+  
+    private
+
+  def preference_params
+    params.require(:preference).permit(:song_sort_order, :artist_sort_order, :allow_create_artists, :allow_create_songs)
+  end
+  
 end
