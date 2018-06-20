651167b3385271e2a872d9c19a28ce3fff677168
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-20 14:09:55 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,7 +1,11 @@
 class Preference < ActiveRecord::Base
 
   def self.song_order
-    Songs.all.order(:name)
+    if Preference.last.song_sort_order == "ASC"
+      Songs.all.order(:name)
+    elsif Preference.last_song_sort_order == "DESC"
+      Songs.all.order(:name :desc)
+    end
   end
   
   def self.artist_order
